41e3702d66cdc846e9452c1237fb7aad98b45177
Ishan Gajurel <ishan.gajurel@verscend.com>
2019-08-10 13:25:16 -0500

    added as a module by including __init__.py file inside a folder and then import and use it

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,6 +1,16 @@
-from utilities import *
 
-total = module1.add(1,3)
-print(total)
+from utilities.module1 import Utilities
+
+total = Utilities.add(1,3)
+print("SUm=",total)
+
+print("===================")
+
+list_x = [1,2,3,5,4]
+Utilities.findLargestInList(list_x)
+
+
+
+
 
 
--- utilities/module1.py
+++ utilities/module1.py
@@ -1,7 +1,20 @@
-def add(a, b):
-    return a+b
 
-def subtract(a,b):
-    return abs(a-b)
+class Utilities:
+    def add(a, b):
+        return a+b
+
+    def subtract(a,b):
+        return abs(a-b)
+
+    #Write a function that returns the largest element in a list.
+    def findLargestInList(inp_list):
+        outp_list = inp_list
+        largest_number = outp_list[0]
+        for i in range(1,len(inp_list)):
+            if inp_list[i] > largest_number:
+                largest_number = inp_list[i]
+
+        print("Largest Number = ", largest_number)
+        return largest_number
 
 
